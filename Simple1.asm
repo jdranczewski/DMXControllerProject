becdd244eec776fa6fc6bc162bd68446942d0772
Jakub Dranczewski <domdlapsa@gmail.com>
2019-11-04 10:53:02 +0000

    Reserving RAM

--- Simple1.asm
+++ Simple1.asm
@@ -3,6 +3,18 @@
 ; Externals section
 	
 ; Reserving space in RAM
+	; Put the 0th byte in access RAM for ease of access
+	udata_acs	.95
+DMXdata res 1
+ 
+	; Put the actual data across banks 0-2 
+	udata_shr
+d1u	udata	.96
+d1	res	.160
+d2u	udata	0x100
+d2	res	.256
+d3u	udata	0x200
+d3	res	.96
 
 ; Constants
 
