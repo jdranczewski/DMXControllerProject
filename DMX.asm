c42cd00847e85f9001d4320e8fae5d6c795b92fd
Jakub Dranczewski <domdlapsa@gmail.com>
2019-11-04 10:15:10 +0000

    Initial structure Changed project name, configuration points to device (not simulator), added DMX.asm

---
+++ DMX.asm
@@ -0,0 +1,12 @@
+	#include p18f87k22.inc
+
+; Globals section
+	
+; Reserving space in RAM
+
+; Constants
+	
+; Put this somewhere in Program memory
+DMX	code
+
+end
--- Simple1.asm
+++ Simple1.asm
@@ -1,21 +1,20 @@
 	#include p18f87k22.inc
-	
-	code
-	org 0x0
-	goto	start
-	
-	org 0x100		    ; Main code starts here at address 0x100
 
-start
-	movlw 	0x0
-	movwf	TRISB, ACCESS	    ; Port C all outputs
-	bra 	test
-loop	movff 	0x06, PORTB
-	incf 	0x06, W, ACCESS
-test	movwf	0x06, ACCESS	    ; Test for end of loop condition
-	movlw 	0x63
-	cpfsgt 	0x06, ACCESS
-	bra 	loop		    ; Not yet finished goto start of loop again
-	goto 	0x0		    ; Re-run program from start
+; Externals section
+	
+; Reserving space in RAM
+
+; Constants
+
+; Reset to 0	
+rst	code	0
+	goto	setup
+
+
+; Put code somewhere in Program Memory
+main code
+
+setup
+	bra $
 	
 	end
